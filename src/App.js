09c65759bafbb9f16602da79c6e4861792fe0741
VictorRandom <victormbalduino@hotmail.com>
2022-06-24 13:22:18 -0300

    adding textarea and checkbox

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Form() {
     const [formData, setFormData] = React.useState(
-        {firstName: "", lastName: "", email: ""}
+        {firstName: "", lastName: "", email: "", comments: "", isFriendly: true}
     )
     
     /**
@@ -12,10 +12,12 @@ export default function Form() {
     console.log(formData)
     
     function handleChange(event) {
+        const {name, value, type, checked} = event.target
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
-                [event.target.name]: event.target.value
+                // [event.target.name]: event.target.value
+                [name]: type === "checkbox" ? checked : value
             }
         })
     }
@@ -27,19 +29,38 @@ export default function Form() {
                 placeholder="First Name"
                 onChange={handleChange}
                 name="firstName"
+                value={formData.firstName}
             />
             <input
                 type="text"
                 placeholder="Last Name"
                 onChange={handleChange}
                 name="lastName"
+                value={formData.lastname}
             />
             <input
                 type="email"
                 placeholder="Email"
                 onChange={handleChange}
                 name="email"
+                value={formData.email}
             />
+            <textarea 
+                type="text"
+                placeholder="comments"
+                onChange={handleChange}
+                name="comments"    
+                value={formData.comments} 
+            />
+            <input 
+                type="checkbox" 
+                id="isFriendly"
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+            <label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
         </form>
     )
 }
